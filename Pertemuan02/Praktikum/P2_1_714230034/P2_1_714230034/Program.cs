9ef4aa4b192b6830ba23d528ef0ef6f8a25bd990
aqilazafira <hartaticpg@gmail.com>
2024-10-15 16:31:21 +0700

    Praktikum Pertmuan 02

---
+++ Pertemuan02/Praktikum/P2_1_714230034/P2_1_714230034/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_1_714230034
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.Write("Masukan Angka Pertama : ");
+            double input = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Masukan Angka Kedua : ");
+            double input2 = Convert.ToDouble(Console.ReadLine());
+
+            double hasil = input + input2;
+            double hasil2 = input - input2;
+            double hasil3 = input * input2;
+            double hasil4 = input / input2;
+            Console.WriteLine("{0} + {1} = {2} ", input, input2, hasil);
+            Console.WriteLine("{0} - {1} = {2} ", input, input2, hasil2);
+            Console.WriteLine("{0} * {1} = {2} ", input, input2, hasil3);
+            Console.WriteLine("{0} / {1} = {2} ", input, input2, hasil4);
+
+
+
+
+
+        }
+    }
+}
