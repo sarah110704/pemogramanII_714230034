bef3a4add4070b669c3dbb04ff07e4f9fea96e03
sarah110704 <sarahyohana439@gmail.com>
2025-01-06 23:01:33 +0700

    P11

---
+++ Pertemuan 11/latihan/pertemuan09 - Copy/P9_714230034/P9_714230034/controller/Mahasiswa.cs
@@ -0,0 +1,72 @@
+﻿using Mysqlx.Crud;
+using P9_714230034.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P9_714230034.controller
+{
+    internal class Mahasiswa
+    {
+        Koneksi koneksi = new Koneksi();
+        private string npm_mhs;
+
+        public bool Insert(M_mahasiswa mahasiswa)
+        {
+            Boolean status = false;
+            try
+            {
+                koneksi.OpenConnection();
+                koneksi.ExecuteQuery("INSERT INTO t_mahasiswa (npm,nama,angkatan,alamat,email,nohp) VALUES ('" + mahasiswa.Npm + "','" + mahasiswa.Nama + "','" + mahasiswa.Angkatan + "','" + mahasiswa.Alamat + "','" + mahasiswa.Email + "','" + mahasiswa.Nohp + "')");
+                status = true;
+                MessageBox.Show("Data berhasil ditambahkan", "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                koneksi.CloseConnection();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Gagal Insert", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return status;
+        }
+        public bool Update(M_mahasiswa mahasiswa, string npm_mhs)
+        {
+            bool status = false;
+            try
+            {
+                koneksi.OpenConnection();
+                koneksi.ExecuteQuery("UPDATE t_mahasiswa SET nama='" + mahasiswa.Nama + "'," + "angkatan='" + mahasiswa.Angkatan + "'," + "alamat='" + mahasiswa.Alamat + "'," + "email='" + mahasiswa.Email + "'," + "nohp='" + mahasiswa.Nohp + "' WHERE npm = '" + npm_mhs + "'");
+                MessageBox.Show("Data berhasil diubah", "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                status = true;
+                koneksi.CloseConnection();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Gagal Update", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            return status;
+        }
+
+
+        public bool Delete(string npm_mhs)
+        {
+            Boolean status = false;
+            try
+            {
+                koneksi.OpenConnection();
+                koneksi.ExecuteQuery("DELETE FROM t_mahasiswa WHERE npm='" + npm_mhs + "'");
+                status = true;
+                MessageBox.Show("Data berhasil dihapus", "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                koneksi.CloseConnection();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Gagal Hapus", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return status;
+        }
+    }
+}
---
+++ Pertemuan 11/latihan/pertemuan09 - Copy/P9_714230034/P9_714230034/view/ParentForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P9_714230034.view
+{
+    public partial class ParentForm : Form
+    {
+        public ParentForm()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void dataMahasiswaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form1 formMhs = new Form1();
+            formMhs.MdiParent = this;
+            formMhs.Show();
+        }
+
+        private void dataNilaiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormNilai FrmNilai = new FormNilai();
+            FrmNilai.MdiParent = this;
+            FrmNilai.Show();
+        }
+    }
+}
